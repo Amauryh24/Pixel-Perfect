f4206c4da5b324eeafbe6e1a678bfb556f81ff14
Amaury <amaury.henrottay@gmail.com>
2020-07-04 16:46:53 +0200

    ex 1 full completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ for (let i = 1; i <= 22; i++) {
   newA.setAttribute("href", "#" + i);
   newLi.appendChild(newA);
   newA.style.background = randomcolor();
+
   let contentA = document.createTextNode(i);
   newA.appendChild(contentA);
 }
@@ -24,5 +25,5 @@ let lastA = document.createElement("a");
 lastA.setAttribute("href", "#nav");
 lastLi.appendChild(lastA);
 lastA.style.background = randomcolor();
-let lastContentA = document.createTextNode("^");
+let lastContentA = document.createTextNode("↓");
 lastA.appendChild(lastContentA);
